e9943fcfe22f328bb3c3c5bc34406d38a540e929
DESKTOP-PS34K9Q\vantan <0dm77t14388098z@ezweb.ne.jp>
2022-09-13 12:22:26 +0900

    Result画面

---
+++ Assets/Sawada/PulseScore.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class PulseScore : MonoBehaviour
+{
+    [SerializeField] float _scoreChangeInterval = 0.5f;
+    Text _scoreText = default;
+
+    int _maxScore = 99999;
+    int _score = 0;
+
+    void Start()
+    {
+        _scoreText = GetComponent<Text>();
+    }
+
+
+    public void PluseScore(int score)
+    {
+        int tempScore = _score;
+        _score = Mathf.Min(_score + score, _maxScore);
+
+
+        if (tempScore != _maxScore)
+        {
+
+            DOTween.To(() => tempScore,
+                x => tempScore = x,
+                _score,
+                _scoreChangeInterval)
+                .OnUpdate(() => _scoreText.text = tempScore.ToString("00000")).SetEase(Ease.InOutQuad)
+                .OnComplete(() => _scoreText.text = _score.ToString("00000"));
+
+        }
+    }
+}
